aaf3b3ad3f32d51f88fe970f71af192b2ae53c55
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-10-18 13:07:40 +0200

    render a chessboard using chessground

--- src/view.ts
+++ src/view.ts
@@ -1,9 +1,21 @@
 import { h, VNode } from 'snabbdom';
+import { Chessground } from 'chessground';
 
 import { Ctrl } from './ctrl.js';
 
 export function view(ctrl: Ctrl): VNode {
   return h('div', [
     h('h1', 'Hello world'),
+    h('div.cg-wrap', {
+      hook: {
+        insert: viewChessground,
+        postpatch: viewChessground
+      }
+    })
   ]);
+}
+
+function viewChessground(cgWrap: VNode) {
+  const el = cgWrap.elm as HTMLElement;
+  const cg = Chessground(el, {});
 }
