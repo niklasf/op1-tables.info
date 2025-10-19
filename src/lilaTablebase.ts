a32b87ba9ecca8c7a911dd7ca1052aeaaa044f37
Niklas Fiekas <niklas.fiekas@backscattering.de>
2025-10-19 16:16:06 +0200

    add types and utils for tablebase xhr

---
+++ src/lilaTablebase.ts
@@ -0,0 +1,21 @@
+interface LilaTablebaseResponse extends LilaTablebasePosInfo {
+  moves: LilaTablebaseMove[];
+}
+
+interface LilaTablebaseMove extends LilaTablebasePosInfo {
+  uci: string;
+  san: string;
+  zeroing: boolean;
+}
+
+interface LilaTablebasePosInfo {
+  checkmate: boolean;
+  stalemate: boolean;
+  insufficient_material: boolean;
+  dtz: number | null;
+  dtm: number | null;
+  dtc: number | null;
+  category: LilaTablebaseCategory;
+}
+
+type LilaTablebaseCategory = 'win' | 'unknown' | 'syzygy-win' | 'maybe-win' | 'cursed-win' | 'draw' | 'blessed-loss' | 'maybe-loss' | 'syzygy-loss' | 'loss';
